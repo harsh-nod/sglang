de1f3d2b8c2f557540d563d35e6cd8453fbfa9f1
Stanley Winata <stanley@nod-labs.com>
2025-02-10 04:09:46 +0000

    [Wave] Adding logit_cap and layer scaling to API
    Also add support for the wave backend to the model
    runner. And use Triton decode kernels for now.

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -1402,6 +1402,10 @@ class ModelRunner:
                 from sglang.srt.layers.attention.triton_backend import TritonAttnBackend
 
                 return TritonAttnBackend(self)
+        elif backend_str == "wave":
+            from sglang.srt.layers.attention.wave_backend import WaveAttnBackend
+
+            return WaveAttnBackend(self)
         elif backend_str == "torch_native":
             from sglang.srt.layers.attention.torch_native_backend import (
                 TorchNativeAttnBackend,
