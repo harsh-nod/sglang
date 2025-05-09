3033222ca96f76fc89aa254ea58e547f95845dcd
Ivan Butygin <ivan.butygin@gmail.com>
2025-05-09 16:14:33 +0200

    fixes
    Signed-off-by: Ivan Butygin <ivan.butygin@gmail.com>

--- python/sglang/srt/layers/attention/wave_backend.py
+++ python/sglang/srt/layers/attention/wave_backend.py
@@ -7,7 +7,7 @@ import torch
 import triton
 import triton.language as tl
 
-from sglang.srt.layers.attention import AttentionBackend
+from sglang.srt.layers.attention.base_attn_backend import AttentionBackend
 from sglang.srt.layers.attention.utils import create_flashinfer_kv_indices_triton
 from sglang.srt.layers.dp_attention import get_attention_tp_size
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch, ForwardMode
